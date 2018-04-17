eee474a4efc39394266080f2738859d2bd8c6f84
Ratson <github@ratson.name>
2018-04-17 23:18:03 +0800

    Add README

--- update.py
+++ update.py
@@ -43,7 +43,7 @@ async def prepare_venv(django_version):
     await venv_queue.put(django_version)
     print(django_version)
 
-    venv_path = tmp_path.joinpath(f'vevn_{django_version}')
+    venv_path = tmp_path.joinpath(f'venv_{django_version}')
     python_path = venv_path.joinpath('bin/python')
     pip_path = venv_path.joinpath('bin/pip')
     django_admin_path = venv_path.joinpath('bin/django-admin.py')
@@ -66,11 +66,11 @@ async def prepare_venv(django_version):
 def repo_run_command(*args, check=False):
     process = subprocess.run(args, cwd=repo_path, stdout=subprocess.PIPE,
                              check=check)
-    return process.stdout
+    return process.stdout.decode(sys.getfilesystemencoding()).strip()
 
 
 def prepare_branch(django_version):
-    venv_path = tmp_path.joinpath(f'vevn_{django_version}')
+    venv_path = tmp_path.joinpath(f'venv_{django_version}')
     django_admin_path = venv_path.joinpath('bin/django-admin.py').resolve()
     branch_name = f'django/v{django_version}'
     try:
@@ -118,16 +118,43 @@ async def prepare_branches():
         repo_queue.task_done()
 
 
+def iter_table_lines(django_versions):
+    last_version = None
+    for version in sorted(django_versions, key=LooseVersion):
+        if last_version is None:
+            yield f'| {version} | - | - |'
+        else:
+            branch_base = f'django/v{last_version}'
+            branch_head = f'django/v{version}'
+            version_range = '...'.join([branch_base, branch_head])
+            compare_url = f'https://github.com/ratson/django-diff/compare/{version_range}'
+            stats = repo_run_command('git', 'diff', '--shortstat',
+                                     branch_base, branch_head)
+            yield f'| {version} | [{version_range}]({compare_url}) | {stats} |'
+        last_version = version
+
+
 def main():
     django_versions = list(iter_django_versions())
-    loop = asyncio.get_event_loop()
-
     repo_path.mkdir(parents=True, exist_ok=True)
+
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.gather(
         prepare_repo(), prepare_branches(),
         *map(prepare_venv, django_versions)))
-
     loop.close()
+
+    with root_path.joinpath('README.md').open() as f:
+        readme_content = f.read()
+    m = re.match(r'(.+\| -+ \| -+ \| -+ \|\s+).*\|[^|]+\| - \| - \|(.+)',
+                 readme_content, re.DOTALL)
+    readme_content = ''.join([
+        m.group(1),
+        '\n'.join(reversed(list(iter_table_lines(django_versions)))),
+        m.group(2),
+    ])
+    with root_path.joinpath('README.md').open('w') as f:
+        f.write(readme_content)
 
 
 if __name__ == '__main__':
